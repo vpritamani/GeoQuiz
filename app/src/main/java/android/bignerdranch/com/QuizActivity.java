20202355fea34740a6f987cf886ac4b989126e0f
vpritamani <44382864+vpritamani@users.noreply.github.com>
2021-01-30 13:41:42 -0800

    Edited QuizActivity.java so that there is a new method called showToastAtTop which displays the given display at the top. Overall, this code completes the challenge of customizing the toast to show at the top instead of the bottom of the screen

--- app/src/main/java/android/bignerdranch/com/QuizActivity.java
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -4,6 +4,7 @@ package android.bignerdranch.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,17 +23,24 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
-                    public void onClick (View v){
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            public void onClick (View v){
+                showToastAtTop(R.string.correct_toast);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v){
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                showToastAtTop(R.string.incorrect_toast);
             }
         });
 
+    }
+
+    // Helper method - receives input of what to display, then displays the toast at the top of the screen
+    private void showToastAtTop(int toDisplay){
+        Toast toast = Toast.makeText(QuizActivity.this, toDisplay, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP,0,200);
+        toast.show();
     }
 }
